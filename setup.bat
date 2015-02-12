69b7f77be2d740b6cff02b443b1bee3001385827
mantislin <mntslin@gmail.com>
2015-02-13 00:15:16 +0800

    update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,28 +1,52 @@
 @echo off
 setlocal
 
-if not exist "%dotfiles%" "" set "dotfiles=%~sdp0"
+if not exist "%dotfiles%" set "dotfiles=%~dp0"
 
-pushd "%dotfiles%"
+pushd "%dotfiles%" || (
+    echo/System cannot find "%dotfiles%".
+    goto :eof
+)
 
-set "extfiles="
-set "extfiles=%extfiles%:%~nx0"
-set "extfiles=%extfiles%:setup.command"
-set "extfiles=%extfiles%:.DS_Store"
-set "extfiles=%extfiles%:Thumbs.db"
+set "extfiles=%~nx0"
+
 set "extfiles=%extfiles%:.git"
-set "extfiles=%extfiles%:.gitignore"
+rem set "extfiles=%extfiles%:bin"
 
-for /f %%a in ('dir/b/a:-d') do (
+set "extfiles=%extfiles%:.DS_Store"
+set "extfiles=%extfiles%:.gitignore"
+rem set "extfiles=%extfiles%:.zshrc"
+rem set "extfiles=%extfiles%:.gemrc"
+rem set "extfiles=%extfiles%:.bashrc"
+rem set "extfiles=%extfiles%:.bash_profile"
+set "extfiles=%extfiles%:setup.command"
+set "extfiles=%extfiles%:Thumbs.db"
+
+for /f %%a in ('dir/b/a:d "%dotfiles%"') do (
     echo/%extfiles%|find /i "%%a" >nul 2>nul && (
         echo/%%a>nul
     ) || (
-        echo/##### %%a
-        echo/%%a
+        echo/
+        echo/########## %%a ##########
+        if "%%a" == ".vimperator" (
+            mklink /j "%userprofile%\vimperator" "%dotfiles%\%%a"
+        ) else (
+            mklink /j "%userprofile%\%%a" "%dotfiles%\%%a"
+        )
     )
 )
-for /f %%a in ('dir/b/a:d') do (
-    echo/DIR: %%a
+for /f %%a in ('dir/b/a:-d "%dotfiles%"') do (
+    echo/%extfiles%|find /i "%%a" >nul 2>nul && (
+        echo/%%a>nul
+    ) || (
+        echo/
+        echo/########## %%a ##########
+        if "%%a" == "vimrc" (
+            mklink "%userprofile%\_vimrc" "%dotfiles%\%%a"
+        ) else (
+            mklink "%userprofile%\%%a" "%dotfiles%\%%a"
+        )
+    )
 )
 
 popd
